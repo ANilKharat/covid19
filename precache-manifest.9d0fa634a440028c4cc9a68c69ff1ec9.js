05f425368338efa4699ea46904b4713f12cd515b
ANilKharat <anilkharat693@gmail.com>
2020-04-15 23:20:36 +0530

    Updates

---
+++ precache-manifest.9d0fa634a440028c4cc9a68c69ff1ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f6f54f1cb695522559e179ebadbfe0",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f0d687e33b9730e1266a",
+    "url": "/covid19/static/css/main.af4be205.chunk.css"
+  },
+  {
+    "revision": "f7fc4dd4514f70599ae1",
+    "url": "/covid19/static/js/2.960ff8de.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.960ff8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d687e33b9730e1266a",
+    "url": "/covid19/static/js/main.e942b9be.chunk.js"
+  },
+  {
+    "revision": "a47f108d1e7f191816aa",
+    "url": "/covid19/static/js/runtime-main.fa4bb7c7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
